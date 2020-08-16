c6ad670961597758dab831a2bb46067c887beaab
anushakp31 <anushakp31@gmail.com>
2020-08-16 11:10:09 +0530

    Modified

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,23 @@
 package org.oop;
+class Point{
 
-public class Point {
-    private double x;
-    private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
+    private final double x;
+    private final double y;
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public  double distance(Point SecondPoint) {
+        double xDistance = SecondPoint.x - this.x;
+        double yDistance = SecondPoint.y - this.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+
+    public double direction(Point secondPoint) {
+        double xDistance = secondPoint.x - this.x;
+        double yDistance = secondPoint.y - this.y;
+        return Math.atan2(yDistance, xDistance);
+
     }
 }
